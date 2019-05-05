eae0b1dbe98828e0ea088e51d94e56da5066a3a9
Chengeng Liu <c.liu75@student.unimelb.edu.au>
2019-05-05 21:35:31 +1000

    Udate

--- Script/run-nectar.sh
+++ Script/run-nectar.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # First source openrc file, which has vars to interact with the openstack api. 
-. ./unimelb-comp90024-group-3-openrc.sh; ansible-playbook --ask-become-pass nectar.yaml
+. ./Instance-Creation/unimelb-comp90024-group-3-openrc.sh; ansible-playbook --ask-become-pass Instance-Creation/nectar.yaml 
+# -i host
