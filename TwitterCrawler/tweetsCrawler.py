75755d199b0d70e700f4c89607d56865c7622dca
Zichun <zhuzichunhogan@gmail.com>
2019-05-08 10:49:01 +1000

    update crawler

--- TwitterCrawler/tweetsCrawler.py
+++ TwitterCrawler/tweetsCrawler.py
@@ -2,6 +2,8 @@ import tweepy
 import couchdb
 import twitter_credentials
 
+dbname = "realtime_tweets"
+
 class TwitterStreamer:
     def __init__(self):
         pass
@@ -14,29 +16,26 @@ class TwitterStreamer:
         auth = tweepy.OAuthHandler(twitter_credentials.consumer_key, twitter_credentials.consumer_secret)
         auth.set_access_token(twitter_credentials.access_token, twitter_credentials.access_token_secret)
 
-        # api = tweepy.API(auth)
-        stream = tweepy.Stream(auth, couchdb_stream_listener)
-        stream.filter(locations=[144.5532, -38.2250, 145.5498, -37.5401])
+        api = tweepy.API(auth, wait_on_rate_limit=True)
+        stream = tweepy.Stream(auth=api.auth, listener=couchdb_stream_listener)
+        stream.filter(locations=[113.155, -43.644, 153.637, -10.689])
 
 
-class CouchDBStreamListener:
+class CouchDBStreamListener(tweepy.StreamListener):
     def __init__(self):
         pass
-        # dbname = "realtime_tweets"
-        # self.json = tweepy.utils.import_simplejson()
-        # couch_server = couchdb.Server("http://localhost:5984/")
-        #
-        # if dbname in couch_server:
-        #     self.db = couch_server[dbname]
-        # else:
-        #     self.db = couch_server.create(dbname)
-        #     print("Creat new db:", dbname)
+        self.json = tweepy.utils.import_simplejson()
+        couch_server = couchdb.Server("http://127.0.0.1:5984/")
+
+        if dbname in couch_server:
+            self.db = couch_server[dbname]
+        else:
+            self.db = couch_server.create(dbname)
 
     def on_data(self, data):
+        jdata = self.json.loads(data)
         print(data)
-        return True
-        # jdata = self.json.loads(data)
-        # self.db.save(jdata)
+        self.db.save(jdata)
 
     def on_error(self, status):
         print(status)
